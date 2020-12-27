4507391a4202fa0b7e8d9852b54beaddb8c57c16
subhransuacharya <subhransuacharya1@gmail.com>
2020-12-27 17:08:12 +0530

    added documentation

--- src/main/java/com/subhransu/CryptoStreamAnalysis.java
+++ src/main/java/com/subhransu/CryptoStreamAnalysis.java
@@ -30,6 +30,7 @@ public class CryptoStreamAnalysis {
         String bootstrapServer;
         String groupId;
 
+        //all the window and slid durations of all 3 analyses
         //time is in minutes
         final Integer BATCH_DURATION = 1;
 
@@ -40,14 +41,7 @@ public class CryptoStreamAnalysis {
         final Integer WINDOW_DURATION_3 = 10;
         final Integer SLIDE_DURATION_3 = 10;
 
-//        final Integer WINDOW_DURATION_1 = 10;
-//        final Integer SLIDE_DURATION_1 = 1;
-//        final Integer WINDOW_DURATION_2 = 10;
-//        final Integer SLIDE_DURATION_2 = 1;
-//        final Integer WINDOW_DURATION_3 = 10;
-//        final Integer SLIDE_DURATION_3 = 1;
-
-
+        //parsing the arguments into local variables, if all arguments are not present return Exception
         if (args.length < 3)
             throw new RuntimeException("Not Enough arguments. Provide <bootstrapServer> <topicName> <groupId>");
         else {
@@ -56,9 +50,10 @@ public class CryptoStreamAnalysis {
             groupId = args[2];
         }
 
+        //the spark conf object, the master will be set to yarn in commandline
         SparkConf sparkConf = new SparkConf().setAppName("CryptoStreamAnalysis").setMaster("local[*]");
 
-
+        //properties to connect to the kafka broker
         Map<String, Object> kafkaParams = new HashMap<>();
         kafkaParams.put("bootstrap.servers", bootstrapServer);
         kafkaParams.put("key.deserializer", StringDeserializer.class);
@@ -67,9 +62,16 @@ public class CryptoStreamAnalysis {
         kafkaParams.put("auto.offset.reset", "latest");
         kafkaParams.put("enable.auto.commit", true);
 
+        //list of topics to subscribe to
         Collection<String> topics = Arrays.asList(topicName);
+
+        //creating JavaStreamingContext object using the spark conf defined earlier with a batch duration of 1 min
         JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.minutes(BATCH_DURATION));
+
+        //setting the logging level to warn to remove unnecessary logs
         Logger.getRootLogger().setLevel(Level.WARN);
+
+        //creating a DStream using KafkaUtils and the kafka connection properties
         JavaInputDStream<ConsumerRecord<String, String>> stream =
                 KafkaUtils.createDirectStream(
                         jssc,
@@ -77,40 +79,53 @@ public class CryptoStreamAnalysis {
                         ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
                 );
 
-
+        //as we need only the value of the kafka stream we map the value into a new DStream
         JavaDStream<String> stringJsonStream = stream.map(record -> record.value());
 
+        //the DStream of JSON String is mapped into object of StockInfo
         JavaDStream<StockInfo> stockInfoJavaDStream = stringJsonStream.map(jsonLine -> {
             ObjectMapper mapper = new ObjectMapper();
             TypeReference<StockInfo> mapType = new TypeReference<StockInfo>() {
             };
             return mapper.readValue(jsonLine, mapType);
         });
-        stockInfoJavaDStream.cache();
-        stockInfoJavaDStream.print();
-//        >>                                    1
 
+        //the resulting DStream of StockInfo is cached as it is to be used by all 3 analyses
+        //same records cannot be read by executor 3 times as they belong to same consumer group
+        stockInfoJavaDStream.cache();
+
+        //the data in 1 min batch is printed on the console
+        stockInfoJavaDStream.print();
+
+
+        //>> Analysis 1 - Moving Closing Average of all stocks in last 10 mins printed every 5 minutes
+
+        //creates a DStream of stockName and closing value from the parsed object
         JavaPairDStream<String, Double> stockAndClosingValue =
                 stockInfoJavaDStream.mapToPair(stockInfo ->
                         new Tuple2<>(stockInfo.getSymbol(), stockInfo.getPriceData().getClose()));
 
+        //creates a DStream based on window duration and slide duration that adds all closing prices in last 10 mins
+        // and slides every 5 mins
         JavaPairDStream<String, Double> stockAndAggregatedClosingValue = stockAndClosingValue
                 .reduceByKeyAndWindow(new Function2<Double, Double, Double>() {
                     @Override
-                    public Double call(Double closeValue1, Double closeValue2) throws Exception {
-                        return closeValue1 + closeValue2;
+                    public Double call(Double closingValue1, Double closingValue2) throws Exception {
+                        return closingValue1 + closingValue2;
                     }
                 }, Durations.minutes(WINDOW_DURATION_1), Durations.minutes(SLIDE_DURATION_1));
 
+        //calculates the average closing value of a sock in last 10 mins by dividing the total sum by 10
         JavaPairDStream<String, Double> stockAndAverageClosingValue = stockAndAggregatedClosingValue
                 .mapValues(new Function<Double, Double>() {
                     @Override
-                    public Double call(Double arg0) throws Exception {
-                        return arg0 / 10;
+                    public Double call(Double aggregatedClosingValue) throws Exception {
+                        return aggregatedClosingValue / 10;
                     }
                 })
                 .repartition(1);
 
+        //rdd containing stockName and avg closing price is printed to console
         stockAndAverageClosingValue.toJavaDStream().foreachRDD(new VoidFunction<JavaRDD<Tuple2<String, Double>>>() {
             @Override
             public void call(JavaRDD<Tuple2<String, Double>> tuple2JavaRDD) throws Exception {
@@ -124,10 +139,15 @@ public class CryptoStreamAnalysis {
         });
 
 
-//      >>                                 2
+        //>> Analysis 2 - Stock with maximum profit in last 10 min window printed every 5 minutes
+
+        //creates a DStream of stockName and PriceData from the parsed object
         JavaPairDStream<String, PriceData> stockAndPriceData = stockInfoJavaDStream.mapToPair(stock ->
                 new Tuple2<>(stock.getSymbol(), stock.getPriceData()));
 
+        //creates a DStream based on window duration and slide duration,
+        // that adds all closing prices and opening prices  in last 10 mins
+        // and slides every 5 mins
         JavaPairDStream<String, PriceData> stockAndAggregatedPriceData = stockAndPriceData
                 .reduceByKeyAndWindow(new Function2<PriceData, PriceData, PriceData>() {
                     @Override
@@ -139,6 +159,8 @@ public class CryptoStreamAnalysis {
                     }
                 }, Durations.minutes(WINDOW_DURATION_2), Durations.minutes(SLIDE_DURATION_2));
 
+        //creates a new DStream by calculating the difference between summed closing and opening prices
+        //resulting in the profitvalue for each stock in the last 10 min window
         JavaPairDStream<String, Double> stockAndProfit = stockAndAggregatedPriceData
                 .mapValues(new Function<PriceData, Double>() {
                     @Override
@@ -147,6 +169,7 @@ public class CryptoStreamAnalysis {
                     }
                 });
 
+        //finds the stock with maximum profit by doing a reduce operation across all rdd and resulting in 1 rdd
         JavaPairDStream<String, Double> stockAndMaxProfit = stockAndProfit.reduce((stock1, stock2) -> {
             if (stock1._2 < stock2._2)
                 return stock2;
@@ -154,22 +177,28 @@ public class CryptoStreamAnalysis {
         }).mapToPair(stringDoubleTuple2 -> new Tuple2<>(stringDoubleTuple2._1, stringDoubleTuple2._2))
                 .repartition(1);
 
+        //the stock with maximum profit is printed
         stockAndMaxProfit.toJavaDStream().foreachRDD(new VoidFunction<JavaRDD<Tuple2<String, Double>>>() {
             @Override
             public void call(JavaRDD<Tuple2<String, Double>> tuple2JavaRDD) throws Exception {
                 tuple2JavaRDD.foreach(new VoidFunction<Tuple2<String, Double>>() {
                     @Override
-                    public void call(Tuple2<String, Double> tuple2) throws Exception {
-                        System.out.println("Stock with highest profit in last interval is " + tuple2._1 + " with a profit of " + tuple2._2);
+                    public void call(Tuple2<String, Double> stockNameAndProfit) throws Exception {
+                        System.out.println("Stock with highest profit in last interval is " + stockNameAndProfit._1 + " with a profit of " + stockNameAndProfit._2);
                     }
                 });
             }
         });
 
-//        >>                                            3
+        //>> Analysis 3 - Stock traded in maximum volumes in last 10 min window printed every 10 minutes
+
+        //creates a DStream of stockName and PriceData from the parsed object
         JavaPairDStream<String, PriceData> stockAndVolumeData = stockInfoJavaDStream.mapToPair(stock ->
                 new Tuple2<>(stock.getSymbol(), stock.getPriceData()));
 
+        //creates a DStream based on window duration and slide duration,
+        // that adds the absolute value of volumes of each stock in last 10 mins
+        // and slides every 10 mins
         JavaPairDStream<String, PriceData> stockAndAggregatedVolumeDataInPriceData = stockAndPriceData
                 .reduceByKeyAndWindow(new Function2<PriceData, PriceData, PriceData>() {
                     @Override
@@ -180,32 +209,37 @@ public class CryptoStreamAnalysis {
                     }
                 }, Durations.minutes(WINDOW_DURATION_3), Durations.minutes(SLIDE_DURATION_3));
 
+        //creates a new dstream of stockName and aggregated absolute volume for that stock
         JavaPairDStream<String, Double> stockAndAggregatedVolumeData2 = stockAndAggregatedVolumeDataInPriceData
                 .mapToPair(stock -> new Tuple2<String,Double>(stock._1, stock._2.getVolume()));
 
+        //creates a new dstream containing the stock with maximum volume in all rdds and returns repartitions to 1 partition
         JavaPairDStream<String,Double> stockAndMaxVolume = stockAndAggregatedVolumeData2
                 .reduce((stock1, stock2) -> {
                     if (stock1._2 < stock2._2)
                         return stock2;
                     else return stock1;
-                }).mapToPair(stringDoubleTuple2 -> new Tuple2<>(stringDoubleTuple2._1, stringDoubleTuple2._2))
+                }).mapToPair(stockNameAndVolume -> new Tuple2<>(stockNameAndVolume._1, stockNameAndVolume._2))
                 .repartition(1);
 
+        //prints the stockName which has been traded in maximum volume
         stockAndMaxVolume.toJavaDStream().foreachRDD(new VoidFunction<JavaRDD<Tuple2<String, Double>>>() {
             @Override
-            public void call(JavaRDD<Tuple2<String, Double>> tuple2JavaRDD) throws Exception {
-                tuple2JavaRDD.foreach(new VoidFunction<Tuple2<String, Double>>() {
+            public void call(JavaRDD<Tuple2<String, Double>> stockNameAndMaxVolumeTupleJavaRDD) throws Exception {
+                stockNameAndMaxVolumeTupleJavaRDD.foreach(new VoidFunction<Tuple2<String, Double>>() {
                     @Override
-                    public void call(Tuple2<String, Double> tuple2) throws Exception {
-                        System.out.println("Stock with highest volume trade in last interval is " + tuple2._1 + " , with volume " + tuple2._2);
+                    public void call(Tuple2<String, Double> stockNameAndMaxVolumeTuple) throws Exception {
+                        System.out.println("Stock with highest volume trade in last interval is " + stockNameAndMaxVolumeTuple._1 + " , with volume " + stockNameAndMaxVolumeTuple._2);
                     }
                 });
             }
         });
 
-
+        //starts the streaming program
         jssc.start();
+        //as it is an infintely running program, it waits for termination signal from user
         jssc.awaitTermination();
+        //upon receiving the termination signal stops the streaming program
         jssc.close();
 
     }
